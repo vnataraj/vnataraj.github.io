a8db262fa3396f463d24723645b29f164f0c878b
Vipul Nataraj <vnataraj@purdue.edu>
2014-04-30 17:55:49 -0400

    spring cleaning

--- header.php
+++
@@ -1,6 +0,0 @@
-<!doctype html>
-<head>
-<link rel="stylesheet" href="help.css" media="screen" type='text/css' />
-	<link href='http://fonts.googleapis.com/css?family=Fredoka+One|Open+Sans:400,700' rel='stylesheet' type='text/css'>
-</head>
-</html>
