ebd308917700903ed9ade0de46ed27a4b341007d
Vipul Nataraj <vnataraj@purdue.edu>
2014-05-11 18:21:51 -0400

    test again

--- pacman/Game.js
+++ pacman/Game.js
@@ -564,7 +564,8 @@ function welcomeScreen(){
 	ctx.font = "20px monospace";
 	ctx.fillText("Press s to start", CANVAS_WIDTH/2, 220);
 	ctx.font = "14px monospace";
-	ctx.fillText("FORKED FROM PACMAN BY: ZI WANG, BINGYING XIA \n AI WRITTEN BY: VIPUL NATARAJ", CANVAS_WIDTH/2 , CANVAS_HEIGHT/20*19);
+	ctx.fillText("FORKED FROM PACMAN BY: ZI WANG, BINGYING XIA", CANVAS_WIDTH/2 , CANVAS_HEIGHT/20*19);
+  ctx.fillText("AI WRITTEN BY: VIPUL NATARAJ", CANVAS_WIDTH/2, CANVAS_HEIGHT/20*18);
 
 	welcomePacman = new Pacman(CANVAS_WIDTH/5, CANVAS_HEIGHT/3*2, RIGHT);
 	welcomePacman.radius = 30;
