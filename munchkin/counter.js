3b984ee5407a3e30f67209a107eab0a8ce2aaa37
Vipul Nataraj <vnataraj@purdue.edu>
2014-06-27 02:06:56 -0700

    barebones counter x1

---
+++ munchkin/counter.js
@@ -0,0 +1,11 @@
+function modify_lvl(val) {
+      var lvl = document.getElementById('lvl').value;
+          var new_lvl = parseInt(lvl,10) + val;
+              
+              if (new_lvl < 1) {
+                        new_lvl = 1;
+                            }
+                  
+                  document.getElementById('lvl').value = new_lvl;
+                  return new_lvl;
+}
